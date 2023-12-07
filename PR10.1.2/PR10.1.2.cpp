5d2fdf12135b40e8d4ca714b8a7018e03838ef79
sofiiaml <sofiia.molotiuk.ik.2023@lpnu.ua>
2023-12-08 00:49:16 +0200

    fssf

---
+++ PR10.1.2/PR10.1.2.cpp
@@ -0,0 +1,37 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+void Count(const char* filename, int counts[256]) {
+    FILE* file = fopen(filename, "r");
+
+    if (!file) {
+        printf("Error during opening file\n");
+        return;
+    }
+
+    int s;
+    while ((s = fgetc(file)) != EOF) {
+        if (s == '+' || s == '-' || s == '=') {
+            counts[s]++;
+        }
+    }
+
+    fclose(file);
+}
+
+int main() {
+    char filename[100];
+    printf("Enter the filename: ");
+    scanf("%s", filename);
+
+    int occurrences[256] = { 0 };
+    Count(filename, occurrences);
+
+    // Виведення результатів
+    printf("Occurrences in file '%s':\n", filename);
+    printf("Symbol '+' %d times.\n", occurrences['+']);
+    printf("Symbol '-' %d times.\n", occurrences['-']);
+    printf("Symbol '=' %d times.\n", occurrences['=']);
+
+    return 0;
+}
